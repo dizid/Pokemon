2eab8b875523a395b412a8c601a4fb373dac410d
Marc de Ruijter <marc@dizid.com>
2022-07-17 19:47:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="p-14"> 
+    <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Pokemon picker</router-link>
+</div>
+<router-view>
+
+</router-view>
+
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="w-full flex justify-center">
+  <input type="text" placeholder="Enter Pokemone here"
+  class="mt-10 p-2 border-blue-500 border-2"
+  v-model="text"/>
+</div>
+
+<div class="mt-10 p-4 flex flex-wrap justify-center">
+<div class="ml-4 text-2x text-blue-500"
+v-for="(pokemon, idx) in filteredPokemon" :key="idx">
+<router-link :to="`/about/${urlIdlookup[pokemon.name]}`">
+   {{ pokemon.name}}
+</router-link>
+
+</div>
+</div>
+</template>
+
+<script>
+import {reactive, toRefs, computed} from "vue"
+export default {
+  name: 'Home-2',
+  setup(){
+
+   const state = reactive({
+    pokemons: [],
+    urlIdlookup: {},
+    text: "",
+    filteredPokemon:computed(()=>updatePokemon())
+   })
+
+function updatePokemon() {
+if(!state.text){
+  return[]
+}
+return state.pokemons.filter((pokemon)=>
+pokemon.name.includes(state.text)
+)
+}
+
+  fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+  .then((res) => res.json())
+  .then((data)=> {
+    console.log(data)
+    state.pokemons = data.results
+    state.urlIdlookup = data.results.reduce((acc, cur, idx)=>
+    acc = {...acc, [cur.name]:idx+1}
+    ,{})
+  })
+  return {...toRefs(state)}
+ }
+}
+</script>
